fabeb888aa18cbad2eda748a75cce112679fdd66
Oleksiy <oleksiygolocnya@gmail.com>
2022-09-05 13:30:31 +0300

    task complete

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.Service.CarService;
+import web.service.CarService;
 import web.model.Car;
 
 import java.util.ArrayList;
